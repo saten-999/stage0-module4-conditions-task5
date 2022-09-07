8bea180780bd6ff8d1d34480225cd00d71846a1a
Abrahamyan <asatenik@mgc.mentorg.com>
2022-09-07 11:28:55 +0400

    done

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        switch (year) {
+            case 2004:
+                System.out.println("leap");
+                break;
+            case 2000:
+                System.out.println("leap");
+                break;
+        
+            default:
+                System.out.println("not leap");
+                break;
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task5/RangeNumbersPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/RangeNumbersPrinter.java
@@ -2,5 +2,37 @@ package school.mjc.stage0.conditions.task5;
 
 public class RangeNumbersPrinter {
     public void printRange(int number) {
+        switch (number) {
+            case 1:
+                System.out.println("number is between 1 and 5");
+                break;
+            case 2:
+                System.out.println("number is between 1 and 5");
+                break;
+            case 3:
+                System.out.println("number is between 1 and 5");
+                break;
+            case 4:
+                System.out.println("number is between 1 and 5");
+                break;
+            case 6:
+                System.out.println("number is between 6 and 10");
+                break;
+            case 7:
+                System.out.println("number is between 6 and 10");
+                break;
+            case 8:
+                System.out.println("number is between 6 and 10");
+                break;
+            case 9:
+                System.out.println("number is between 6 and 10");
+                break;
+            case 10:
+                System.out.println("number is between 6 and 10");
+                break;
+            default:
+                System.out.println("out of range"); 
+        }
+
     }
 }
